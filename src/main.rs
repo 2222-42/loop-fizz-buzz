dd6996166d8a5b2c285164bd8040e66ecb774d7f
2222-42 <stephen0617@me.com>
2022-08-12 17:09:25 +0900

    fix(main): 15を出力させるようにする
    next()を呼び出す必要があることに器付ける

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,16 @@ fn main() {
     let fizzes = repeat("").take(2).chain(once("fizz")).cycle();
     let buzzes = repeat("").take(4).chain(once("buzz")).cycle();
     let fizzbuzzes = fizzes.zip(buzzes);
-    let mut counter : i8 = 0;
+    let mut counter: i8 = 1;
     let mut fizbuz_iter = fizzbuzzes.into_iter();
     loop {
         let element = fizbuz_iter.next();
+
         match (counter, element) {
+            (15, Some(("", ""))) => {
+                println!("15");
+                break;
+            }
             (i, Some(("", ""))) => {
                 println!("{}", i);
             }
@@ -19,7 +24,10 @@ fn main() {
                 break;
             }
         }
-        counter += 1; 
+        counter += 1;
+        if counter == i8::MAX {
+            fizbuz_iter.next();
+            fizbuz_iter.next();
+        }
     }
-
 }
