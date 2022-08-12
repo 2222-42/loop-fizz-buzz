648cacd67da94fa0673df42ee1eb5cea2cb874e4
2222-42 <stephen0617@me.com>
2022-08-12 16:16:46 +0900

    feat(main): loop
    release ビルドなら通る

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,22 @@ fn main() {
     let fizzes = repeat("").take(2).chain(once("fizz")).cycle();
     let buzzes = repeat("").take(4).chain(once("buzz")).cycle();
     let fizzbuzzes = fizzes.zip(buzzes);
-    let fizz_buzz = (1..100).zip(fizzbuzzes).map(|tuple| match tuple {
-        (i, ("", "")) => i.to_string(),
-        (_, (fizz, buzz)) => format!("{}{}", fizz, buzz),
-    });
+    let mut counter : i8 = 0;
+    let mut fizbuz_iter = fizzbuzzes.into_iter();
+    loop {
+        let element = fizbuz_iter.next();
+        match (counter, element) {
+            (i, Some(("", ""))) => {
+                println!("{}", i);
+            }
+            (_, Some((fizz, buzz))) => {
+                println!("{}{}", fizz, buzz);
+            }
+            (_, None) => {
+                break;
+            }
+        }
+        counter += 1; 
+    }
 
-    println!("{}", fizz_buzz.collect::<Vec<_>>().join("\n"));
 }
